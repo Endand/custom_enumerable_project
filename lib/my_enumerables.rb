e09c4564d8f0d18c558ad0116718908271ecf3b6
Endand <endand2898@gmail.com>
2024-03-15 21:57:10 -0400

    Added Array#my_each method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,10 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for i in 0...self.length
+      yield(self[i])
+    end
+    self
+  end
 end
